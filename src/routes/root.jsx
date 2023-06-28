993990d110588746e2f85c759cccd9b9dccef7e9
TechGlue <garcialuis24@hotmail.com>
2023-06-28 13:08:41 -0700

    Content added

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,12 +4,14 @@ import Navbar from "./components/navbar.jsx";
 export default function Root() {
   return (
     <>
-      <div>
-        <div class="bg-skin-fill text-skin-base h-screen flex flex-col justify-center items-center">
-          <div class="flex justify-center ">
-          </div>
+      <div className="bg-skin-fill">
+        <div class="text-skin-base h-screen flex flex-col justify-center items-center">
+          <div class="flex justify-center "></div>
           <SplashPage />
         </div>
+        <span class="pb-2 block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          ©2023 Luís Garcia
+        </span>
       </div>
     </>
   );
--- src/routes/splashpage.jsx
+++ src/routes/splashpage.jsx
@@ -1,7 +1,7 @@
 //make this a component
 import React from "react";
 import { Link } from "react-router-dom";
-import image from "./../assets/prof.png";
+import image from "./../assets/square.webp";
 
 export default function SplashPage() {
   return (
@@ -11,32 +11,54 @@ export default function SplashPage() {
           <p className="border-r-white pr-3 text-5xl text-white font-bold">
             Hello, I'm Luís
           </p>
-          <p className="border-r-white pr-3 whitespace-nowrap">
-            {" "}
-            Loo-eece (emphasis on eece){" "}
-          </p>
+          <p className="border-r-white pr-3 text-1xl whitespace-nowrap"> (Loo-eece) </p>
+          <img
+            src={image}
+            alt="my-prof"
+            className="w-60 h-60 mt-1"
+          />
+
+          <div className="mt-5 flex justify-left">
+            <div className="text-3xl grid grid-cols-3 gap-4">
+              <Link to={`/`}>
+                <i class="fa-brands fa-square-github"></i>
+              </Link>
+              <Link to={`/`}>
+                <i class="fa-brands fa-spotify"></i>
+              </Link>
+              <Link to={`/`}>
+                <i class="fa-regular fa-square-envelope"></i>
+              </Link>
+            </div>
+          </div>
         </div>
         <p className="text-3xl mb-2 font-bold">Who am I?</p>
         <p>
           I'm a Washington based Software Engineer. Who mostly works with C# at
-          work but has a strong passion and curisoty for other languages that
-          change the way I approach problems and think.
+          work but has a strong passion and curiosity for other technologies
+          that changes the way I approach problems and overall pushes me to get
+          out of my shell.
         </p>
         <br />
-        <p className="text-3xl mb-2 font-bold">A bit about myself</p>
+        <p className="text-3xl mb-2 font-bold">A bit about my journey</p>
         <p>
-          In 2018, I was introduced to my first programming language, Python,
-          during my first year of university. From that moment on, I was hooked
-          and eagerly delved into the world of software development, absorbing
-          everything I could along the way. My early projects included building
-          a CLI app that could process input and output, configuring my first
-          Linux build, and creating various simple programs that helped me
-          remember the syntax for the different languages I used for my
-          coursework. Like everything, in life the more consistent you stay with
-          something the greater chances you have of getting better at it. This
-          consistency lead to me snagging my self my first internship in 2022,
-          and my first programming job.
+          I'm a Eastern Washington Univeristy Graduate, who back in 2018 was
+          entering their first Computer Science class, and as a person with no
+          technical background, I felt way over my head. Luckily, I stuck
+          through it and has changed my life for the better. Through alot of
+          trial and error, help from my peers, and many hours of coding in my
+          free time on anything big or small. I ended up graduating and starting
+          my first Software Engineering position at Premera Blue Cross in the
+          Fall of 2023.
         </p>
+        <p className="mt-2">
+          Looking back, it feels like it's been a long time since I started my
+          journey, but I really grew to appreciate all the struggles that it
+          took to get here. Right now, my main goal is to grow as a developer by
+          continuing to push myself to learn new things and hopefully help
+          others along the way.
+        </p>
+
         <p className="text-3xl mt-2 mb-2 font-bold">Quick Facts: </p>
         <ul className="list-disc ml-8">
           <li>Linux enthusiast</li>
@@ -45,22 +67,8 @@ export default function SplashPage() {
             I enjoy configruing my terminal and learning new tools to boost my
             productivity
           </li>
-          <li>Split keyboard enthusiast (can't live without it)</li>
+          <li>Split keyboard enjoyer</li>
         </ul>
-
-        <div className="mt-5 flex justify-left">
-          <div className="text-3xl grid grid-cols-3 gap-4">
-            <Link to={`/`}>
-              <i class="fa-brands fa-square-github"></i>
-            </Link>
-            <Link to={`/`}>
-              <i class="fa-brands fa-spotify"></i>
-            </Link>
-            <Link to={`/`}>
-              <i class="fa-regular fa-square-envelope"></i>
-            </Link>
-          </div>
-        </div>
       </div>
     </>
   );
