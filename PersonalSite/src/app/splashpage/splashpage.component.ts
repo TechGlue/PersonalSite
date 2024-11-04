c14109f356589519dad5bdf9ced15cfb861678e9
TechGlue <garcialuis42@outlook.com>
2024-11-04 15:59:56 -0800

    layed out components dirs

---
+++ PersonalSite/src/app/splashpage/splashpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splashpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './splashpage.component.html',
+  styleUrl: './splashpage.component.css'
+})
+export class SplashpageComponent {
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# PersonalSite
-A personal website of mine that I've been working on throughout my years as a student.
