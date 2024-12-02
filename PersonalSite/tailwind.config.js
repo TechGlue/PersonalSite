505339f62f067fdef2defd698a06b64e52ec110b
TechGlue <garcialuis42@outlook.com>
2024-12-02 01:24:50 -0800

    fa icons and hover colors

--- PersonalSite/tailwind.config.js
+++ PersonalSite/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         textSecondary: "#c9d4e6",
         buttonPrimary: "#54626E",
         buttonHover: "#3a5681",
+        iconsHover: "#3a5681",
       },
       keyframes: {
         typing: {
