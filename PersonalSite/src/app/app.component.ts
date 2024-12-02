f25deb81ddf2b7801cbd39770e2e360bfe5f404c
TechGlue <garcialuis42@outlook.com>
2024-12-02 14:27:10 -0800

    stylings to stack navbar upon sm screen

--- PersonalSite/src/app/app.component.ts
+++ PersonalSite/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SplashpageComponent } from './splashpage/splashpage.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [NavbarComponent, RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [NavbarComponent, RouterOutlet, SplashpageComponent, AboutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Luís Garcia';
