a55994ca78f609232a15260cde5f904bbf545924
TechGlue <garcialuis42@outlook.com>
2024-12-05 12:30:39 -0800

    continuing figuring out the design

--- PersonalSite/src/app/education/education.component.ts
+++ PersonalSite/src/app/education/education.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ExperienceComponent } from './experience/experience.component';
 
 @Component({
+  standalone: true,
   selector: 'app-education',
   imports: [ExperienceComponent],
   templateUrl: './education.component.html',
--- PersonalSite/src/app/work/workitem/workitem.component.ts
+++ PersonalSite/src/app/work/workitem/workitem.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-workitem',
-  imports: [],
   templateUrl: './workitem.component.html',
 })
 export class WorkitemComponent {
